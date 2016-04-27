5182abb835a5a5973fb2bfac5bc6f88eb5b5d194
Jared Billig <jared.billig@gmail.com>
2016-04-27 15:10:44 -0400

    added new events and methods for toggleCompleted, toggleVisible, isHidden, and clear

--- js/views/todos.js
+++ js/views/todos.js
@@ -6,9 +6,11 @@ app.TodoView = Backbone.View.extend({
   template: _.template( $('#item-template').html() ),
 
   events: {
-    'dblclick label'  : 'edit',
-    'keypress .edit'  : 'updateOnEnter',
-    'blur     .edit'  : 'close'
+    'click    .toggle'  : 'toggleCompleted',
+    'dblclick label'    : 'edit',
+    'click    .destroy' : 'clear',
+    'keypress .edit'    : 'updateOnEnter',
+    'blur     .edit'    : 'close'
   },
 
   // The TodoView listens for changes to its model, re-rendering. Since there's
@@ -16,12 +18,34 @@ app.TodoView = Backbone.View.extend({
   // app, we set a direct reference to the model for convenience
   initialize: function() {
     this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'visible', this.toggleVisible);
   },
 
   render: function() {
     this.$el.html( this.template( this.model.attributes ) );
+
+    this.$el.toggleClass( 'completed', this.model.get('completed') );
+    this.toggleVisible();
+
     this.$input = this.$('.edit');
     return this;
+  },
+
+  toggleVisible: function() {
+    this.$el.toggleClass('hidden', this.isHidden());
+  },
+
+  isHidden: function() {
+    var isCompleted = this.model.get('completed');
+    return (
+      (!isCompleted && app.TodoFilter === 'completed')
+      || (isCompleted && app.TodoFilter === 'active')
+    );
+  },
+
+  toggleCompleted: function() {
+    this.model.toggle();
   },
 
   edit: function() {
@@ -43,5 +67,9 @@ app.TodoView = Backbone.View.extend({
     if (e.which === ENTER_KEY) {
       this.close();
     }
+  },
+
+  clear: function() {
+    this.model.destroy();
   }
 });
