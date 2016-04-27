82fb64f51a8d61a26d62104dc2661889c7b7dce4
Jared Billig <jared.billig@gmail.com>
2016-04-27 12:48:20 -0400

    finished todo model with defaults and toggle function

---
+++ js/models/todo.js
@@ -0,0 +1,19 @@
+var app = app || {};
+
+// Todo Model
+// ----------
+// Has properties 'title' and 'completed'
+
+app.Todo = Backbone.Model.extend({
+  defaults: {
+    title: '',
+    completed: false
+  },
+
+  toggle: function() {
+    this.save({
+      completed: !this.get('completed');
+    });
+  }
+  
+});
